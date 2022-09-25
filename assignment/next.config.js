03322c4b05624487ed42a7193cb44bdab379c274
borsym <borsymatee@gmail.com>
2022-09-25 21:22:46 +0200

    added env var

--- assignment/next.config.js
+++ assignment/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   env: {
-    API_KEY: '75cf132f45mshb3295bd4972ed03p1b85d8jsndc2b5c96ce0e', // DELETE THIS FROM HERE ITS PUBLIC
+    API_KEY: process.env.API_KEY, // TODO check, .env
   },
 };
 
